5a355b376b0d1fc6271a57908e6145d94792ddaa
Nikita Kharin <harin-1990@yandex.ru>
2024-10-16 23:11:40 +0400

    Итоговый проект 5.6.1 (HW-03)

--- Module5/Program.cs
+++ Module5/Program.cs
@@ -7,139 +7,110 @@ class MainClass
 
     public static void Main(string[] args)
     {
-        Console.WriteLine("Напишите что-то");
-
-        var str = Console.ReadLine();
-
-        Console.WriteLine("Укажите глубину эха");
-
-        var deep = int.Parse(Console.ReadLine());
-
-
-        Echo(str, deep);
+        var GetInfo = UserInfoGet();
+        UserInfo(GetInfo);
 
     }
 
-    static void Echo(string saidworld, int deep)
+
+
+    static (string Name, string LastName, int Age, string[] Pets, string[] Colors) UserInfoGet()
     {
-        var modif = saidworld;
+        Console.Write("Введите ваше имя: ");
+        string Name = Console.ReadLine();
 
-        if (modif.Length > 2)
+        Console.Write("Введите вашу фамилию: ");
+        string LastName = Console.ReadLine();
+
+        int Age = GetControlNum("Введите ваш возраст: ");
+
+        string[] Pets = null;
+        Console.Write("У вас есть питомец? (Да или нет): ");
+        bool hasPet = Console.ReadLine()?.ToLower() == "да"; 
+
+        if (hasPet)
         {
-
-            modif = modif.Remove(0, 2);
+            int petCount = GetControlNum("Введите количество питомцев: ");
+            Pets = GetPets(petCount);
         }
 
-        Console.WriteLine("..." + modif);
+        int ColorCount = GetControlNum("Введите количество любимых цветов: ");
+        string[] Colors = GetColors(ColorCount);
 
-        if (deep > 1)
+        return (Name, LastName, Age, Pets, Colors);
+    }
+
+
+    static int GetControlNum(string Num)
+    {
+        int number;
+        while (true)
         {
-            Echo(modif, deep - 1);
+            Console.Write(Num);
+            if (int.TryParse(Console.ReadLine(), out number) && number > 0)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Число неверное. Введите целое число положительное число.");
+            }
         }
-
+        return number;
     }
 
 
 
+    static string[] GetPets(int count)
+    {
+        string[] petNames = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"Введите кличку питомца {i + 1}: ");
+            petNames[i] = Console.ReadLine();
+        }
+        return petNames;
+    }
 
-    //public static void Main(string[] args)
-    //{
-
-    //    var arr = new int[] { 1, 2, 3 };
-    //    BigDataOperation(arr);
-
-    //    Console.WriteLine(arr[0]);
-
-    //}
-
-    //static void BigDataOperation(int[] arr)
-    //{
-    //    arr[0] = 4;
-    //}
-
-
-
-
-    //    public static void Main(string[] args)
-    //    {
-
-    //        var (name, age) = ("Никита", 27);
-
-    //        Console.WriteLine("Мое имя: {0}", name);
-    //        Console.WriteLine("Мой возраст: {0}", age);
-
-    //        Console.Write("Введите имя: ");
-    //        name = Console.ReadLine();
-    //        Console.Write("Введите возрас с цифрами:");
-    //        age = Convert.ToInt32(Console.ReadLine());
-
-    //        Console.WriteLine("Ваше имя: {0}", name);
-    //        Console.WriteLine("Ваш возраст: {0}", age);
-
-    //        var favcolors = new string[3];
-
-    //        for (int i = 0; i < favcolors.Length; i++)
-
-    //        {
-    //            favcolors[i] = ShowColor(favcolors);
-    //        }
-
-    //        Console.WriteLine("Ваши любимые цвета"); 
-    //        foreach (var color in favcolors)
-    //        {
-    //            Console.WriteLine(color);
-    //        }
+    static string[] GetColors(int count)
+    {
+        string[] Colors = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"Введите название любимого цвета {i + 1}: ");
+            Colors[i] = Console.ReadLine();
+        }
+        return Colors;
+    }
 
 
-    //        Console.ReadKey();
+    static void UserInfo((string Name, string LastName, int Age, string[] Pets, string[] Colors) userInfo)
+    {
+        Console.WriteLine("Информация о пользователе");
+        Console.WriteLine($"Имя: {userInfo.Name}");
+        Console.WriteLine($"Фамилия: {userInfo.LastName}");
+        Console.WriteLine($"Возраст: {userInfo.Age}");
 
+        if (userInfo.Pets != null)
+        {
+            Console.WriteLine("Клички питомцев: " + string.Join(", ", userInfo.Pets));
+        }
+        else
+        {
+            Console.WriteLine("У пользователя нет питомцев.");
+        }
 
-    //    }
-
-
-
-
-
-
-
-    //    static string ShowColor(string[] favcolors)
-    //    {
-    //        Console.WriteLine("{0}, {1} лет  \nНапишите свой любимый цвет на английском с маленькой буквы" , username, userage);
-    //        var color = Console.ReadLine();
-
-    //        switch (color)
-    //        {
-    //            case "red":
-    //                Console.BackgroundColor = ConsoleColor.Red;
-    //                Console.ForegroundColor = ConsoleColor.Black;
-
-    //                Console.WriteLine("Your color is red!");
-    //                break;
-
-    //            case "green":
-    //                Console.BackgroundColor = ConsoleColor.Green;
-    //                Console.ForegroundColor = ConsoleColor.Black;
-
-    //                Console.WriteLine("Your color is green!");
-    //                break;
-    //            case "cyan":
-    //                Console.BackgroundColor = ConsoleColor.Cyan;
-    //                Console.ForegroundColor = ConsoleColor.Black;
-
-    //                Console.WriteLine("Your color is cyan!");
-    //                break;
-    //            default:
-    //                Console.BackgroundColor = ConsoleColor.Yellow;
-    //                Console.ForegroundColor = ConsoleColor.Red;
-
-    //                Console.WriteLine("Your color is yellow!");
-    //                break;
-
-
-    //        }
-
-    //        return color;
+        Console.WriteLine("Любимые цвета: " + string.Join(", ", userInfo.Colors));
+    }
 }
+
+
+
+
+
+
+
+
 
 
 
